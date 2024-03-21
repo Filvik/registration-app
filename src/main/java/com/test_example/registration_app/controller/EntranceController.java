771224c09ea88001ed2035e9c0033fe55ace7cb0
Filvik <filvik@bk.ru>
2024-03-22 00:04:43 +0200

    login page optimization

--- src/main/java/com/test_example/registration_app/controller/EntranceController.java
+++ src/main/java/com/test_example/registration_app/controller/EntranceController.java
@@ -1,28 +1,19 @@
 package com.test_example.registration_app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 @Controller
 public class EntranceController {
 
-    @GetMapping("/userPage")
-    @PreAuthorize("hasAuthority('User')")
-    public String user() {
-        return "userPage";
-    }
-
-    @GetMapping("/operatorPage")
-    @PreAuthorize("hasAuthority('Operator')")
-    public String operator() {
-        return "operatorPage";
-    }
-
-    @GetMapping("/adminPage")
-    @PreAuthorize("hasAuthority('Administrator')")
-    public String admin() {
-        return "adminPage";
+    @GetMapping("/universalFormPage")
+    @PreAuthorize("hasAnyAuthority('User', 'Operator', 'Administrator')")
+    public String admin(Model model, Authentication authentication) {
+        model.addAttribute("username", authentication.getName());
+        model.addAttribute("authorities", authentication.getAuthorities());
+        return "universalFormPage";
     }
 }
-
--- src/main/java/com/test_example/registration_app/handler/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/test_example/registration_app/handler/CustomAuthenticationSuccessHandler.java
@@ -3,7 +3,6 @@ package com.test_example.registration_app.handler;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import java.io.IOException;
@@ -13,14 +12,7 @@ public class CustomAuthenticationSuccessHandler implements AuthenticationSuccess
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException {
-        if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("User"))) {
-            response.sendRedirect(request.getContextPath() + "/userPage");
-        } else if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("Operator"))) {
-            response.sendRedirect(request.getContextPath() + "/operatorPage");
-        } else if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("Administrator"))) {
-            response.sendRedirect(request.getContextPath() + "/adminPage");
-        } else {
-            response.sendRedirect(request.getContextPath() + "/");
-        }
+            response.sendRedirect(request.getContextPath() + "/universalFormPage");
     }
 }
+
