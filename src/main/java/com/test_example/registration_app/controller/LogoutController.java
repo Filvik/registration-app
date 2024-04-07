984bd1739af9c7854f8695b3e73eedd9df465cff
Filvik <filvik@bk.ru>
2024-04-07 21:09:32 +0200

    feat: update LogoutController

--- src/main/java/com/test_example/registration_app/controller/LogoutController.java
+++ src/main/java/com/test_example/registration_app/controller/LogoutController.java
@@ -3,15 +3,17 @@ package com.test_example.registration_app.controller;
 import com.test_example.registration_app.service.LogoutService;
 import com.test_example.registration_app.service.TokenBlacklistService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @Slf4j
-@RestController
+@Controller
 @RequestMapping("/logout")
 @RequiredArgsConstructor
 @Tag(name = "LogoutController", description = "Контроллер для выхода из системы.")
@@ -21,14 +23,21 @@ public class LogoutController {
     private final LogoutService logoutService;
 
     @PostMapping
-    @Operation(summary = "Выход пользователя", description = "Делает недействительным токен JWT для выхода пользователя из системы.")
-    public ResponseEntity<?> logout(@RequestHeader("Authorization") @Parameter(description = "Токен JWT для аннулирования") String token) {
-        String jti = logoutService.extractJti(token);
-        Long expirationTime = logoutService.extractExpirationTime(token);
-        tokenBlacklistService.blacklistToken(jti, expirationTime - System.currentTimeMillis());
-
-        log.info("User logged out and token invalidated");
-        return ResponseEntity.ok().build();
+    @Operation(summary = "Выход пользователя", description = "Отображает страницу выхода и делает недействительным токен JWT.")
+    public String logout(
+            @RequestHeader("Authorization") String token,
+            Model model) {
+        try {
+            String jti = logoutService.extractJti(token);
+            Long expirationTime = logoutService.extractExpirationTime(token);
+            tokenBlacklistService.blacklistToken(jti, expirationTime - System.currentTimeMillis());
+            log.info("User logged out and token invalidated");
+            model.addAttribute("logoutMessage", "Вы успешно вышли из системы.");
+        } catch (Exception e) {
+            log.error("Ошибка при выходе: ", e);
+            model.addAttribute("logoutError", "Произошла ошибка при попытке выхода.");
+            return "error";
+        }
+        return "logout";
     }
 }
-
